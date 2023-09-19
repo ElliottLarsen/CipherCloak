21d3e699a569882e2f8acca8478b2640781ca276
Elliott Larsen <elliottlrsn@gmail.com>
2023-09-19 16:40:20 -0600

    Encrypt client connection.

---
+++ encrypt_client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "net"
+  "os"
+)
+
+func main() {
+  // Check the arguments passed.
+  if len(os.Args) != 4 {
+    fmt.Fprintln(os.Stderr, "Incorrect command line arguments.  It should be ./client <plaintext> <key> <port>")
+    os.Exit(0)
+  }
+
+  //plaintext := os.Args[1]
+  //key := os.Args[2]
+  port := os.Args[3]
+  // Connect to the encrypt server.
+  conn, err := net.Dial("tcp", "localhost:" + port)
+  if err != nil {
+    fmt.Fprintln(os.Stderr, "Error connecting to the server on port " + port)
+    fmt.Fprintln(os.Stderr, err)
+    os.Exit(1)
+  }
+  defer conn.Close()
+
+}

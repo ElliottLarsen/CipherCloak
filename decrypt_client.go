48146820626ac9de38eeaf899cc1020f16003304
Elliott Larsen <elliottlrsn@gmail.com>
2023-09-22 16:24:43 -0600

    Encrypt client clean up and decrypt client first pass.

--- decrypt_client.go
+++ decrypt_client.go
@@ -9,34 +9,34 @@ import (
 func main() {
   // Check the arguments passed.
   if len(os.Args) != 4 {
-    fmt.Fprintln(os.Stderr, "Incorrect command line arguments.  It should be ./client <plaintext> <key> <port>")
+    fmt.Fprintln(os.Stderr, "Incorrect command line arguments.  It should be ./client <cipher> <key> <port>")
     os.Exit(0)
   }
 
-  plaintext := os.Args[1]
+  cipher := os.Args[1]
   key := os.Args[2]
   port := os.Args[3]
-  // Connect to the encrypt server.
+  // Connect to the decrypt server.
   conn, err := net.Dial("tcp", "localhost:" + port)
   if err != nil {
-    fmt.Fprintln(os.Stderr, "Error connecting to the encrypt server on port " + port)
+    fmt.Fprintln(os.Stderr, "Error connecting to the decrypt server on port " + port)
     fmt.Fprintln(os.Stderr, err)
     os.Exit(1)
   }
   defer conn.Close()
   // Send file names to the server.
-  file_names := plaintext + " " + key
+  file_names := cipher + " " + key
   _, err = conn.Write([]byte(file_names))
   if err != nil {
-    fmt.Fprintln(os.Stderr, "Error sending files to the encrypt server.")
+    fmt.Fprintln(os.Stderr, "Error sending files to the decrypt server.")
     fmt.Fprintln(os.Stderr, err)
     os.Exit(1)
   }
-  // Receive and print the encrypted text from the server.
+  // Receive and print the decrypted message from the server.
   buffer := make([]byte, 200000)
   n, err := conn.Read(buffer)
   if err != nil {
-    fmt.Fprintln(os.Stderr, "Error receiving encrypted text from the server.")
+    fmt.Fprintln(os.Stderr, "Error receiving decrypted message from the decrypt server.")
     fmt.Fprintln(os.Stderr, err)
     os.Exit(1)
   }
